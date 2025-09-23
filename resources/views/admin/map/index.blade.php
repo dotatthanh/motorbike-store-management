7d3362172aba5182923f4f7b1ecaa65b3e602357
Thanh Do Tat <thanhdt@yopaz.vn>
2025-09-23 16:19:00 +0700

    wip

--- resources/views/admin/map/index.blade.php
+++ resources/views/admin/map/index.blade.php
@@ -23,26 +23,15 @@
                 </div>
 
                 <div class="container-map">
-                    {{-- <div class="box-search">
-                        <div class="box-search-content">
-                            <input type="text" placeholder="Tìm kiếm Map4D" id="input-search-map">
-                            <button type="button">
-                                <img src="{{ asset('assets/images/icon-search.svg') }}" alt="">
-                            </button>
-            
-                        </div>
-                        <div class="list-search"></div>
-                    </div> --}}
-                    {{-- <div class="place-info">
+                    <div class="place-info">
                         <button type="button" class="btn-close-place-info">x</button>
                         <div class="fw-bold place-info-name"></div>
+                        <div class="d-block place-info-company-name"></div>
+                        <div class="d-block place-info-email"></div>
                         <div class="d-block place-info-address"></div>
-                        <div class="d-block place-info-latlng"></div>
+                        <div class="d-block place-info-phone-number"></div>
                     </div>
-                    <div id="map" class="w-100" style="height:500px;"></div> --}}
-
-                    <div id="map" style="width:100%;height:500px"></div>
-                    {{-- <div id="store-popup" class="place-info"></div> --}}
+                    <div id="map" class="w-100" style="height:500px;"></div>
                 </div>
             </div>
         </div>
@@ -60,19 +49,6 @@
     <script src="https://api.map4d.vn/sdk/map/js?version=2.6&key={{ config('app.map4d_key') }}&callback=initMap"></script>
 @endsection
     @section('css')
-    <!-- datepicker css -->
-    <link href="{{ asset('/assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" rel="stylesheet"
-        type="text/css">
-    <!-- select2 css -->
-    <link href="{{ asset('assets\libs\select2\select2.min.css') }}" rel="stylesheet" type="text/css">
-
     <link href="{{ asset('assets/css/pages/route-stop-point.css') }}" rel="stylesheet" type="text/css">
-    <style>
-        .mf-info-window-content h4,
-        .mf-info-window-content p {
-            font-family: inherit !important;
-        }
-    </style>
-
 @endsection
 
